10ec67d1a175479fa706d9ac174e12230b513b91
Prachet Pandav <113706290+prachet283@users.noreply.github.com>
2024-08-16 13:43:08 +0530

    Update Regression_Projects_WebApp.py

--- Regression_Projects_WebApp.py
+++ Regression_Projects_WebApp.py
@@ -16,11 +16,11 @@ with open("Preprocessing File/ML-Project-3-House_Price_Prediction/columns.pkl", 
     all_columns_house_price = pickle.load(f)
 with open("Preprocessing File/ML-Project-3-House_Price_Prediction/scaler.pkl", 'rb') as f:
     scaler_house_price = pickle.load(f)
-with open("Best Features/ML-Project-3-House_Price_Prediction/best_features_lr.json", 'r') as file:
+with open("Best Features\ML-Project-3-House_Price_Prediction_Best_Features\best_features_lr.json", 'r') as file:
     best_features_lr_house_price = json.load(file)
-with open("Best Features/ML-Project-3-House_Price_Prediction/best_features_rfr.json", 'r') as file:
+with open("Best Features\ML-Project-3-House_Price_Prediction_Best_Features\best_features_rfr.json", 'r') as file:
     best_features_rfr_house_price = json.load(file)
-with open("Best Features/ML-Project-3-House_Price_Prediction/best_features_xgb.json", 'r') as file:
+with open("Best Features\ML-Project-3-House_Price_Prediction_Best_Features\best_features_xgb.json", 'r') as file:
     best_features_xgb_house_price = json.load(file)
 with open("Models/ML-Project-3-House_Price_Prediction/house_price_prediction_trained_lr_model.sav", 'rb') as f:
     loaded_model_lr_house_price = pickle.load(f)
